19d494d36860921158bd603370d9ac57bf94ed64
samanramezani <ramezani.data@gmail.com>
2025-09-30 17:00:53 +0330

    sixth commit

--- FASTAPI/book.py
+++ FASTAPI/book.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 
 BOOKS = [
-    {'title': "title one" , 'author' : 'author one' , 'category' : 'science'} ,
+    {'title': "title one" , 'author' : 'author one' , 'category' : 'science' } ,
      {'title': "title tow" , 'author' : 'author two' , 'category' : 'science'} ,
       {'title': "title three" , 'author' : 'author three' , 'category' : 'history'} ,
        {'title': "title four" , 'author' : 'author four' , 'category' : 'math'} ,
@@ -18,13 +18,38 @@ BOOKS = [
 async def say_hi():
      return { 'salam' : 'mohsen'}
 
+
+
+
+@app.get("/book") 
+async def show_book():
+     return BOOKS
+
+################## path param ######################################
+
 # @app.get("/books/{dynamic_param}") 
 # async def read_all_books(dynamic_param):
 #         return {'dynamic_param' : dynamic_param}
 
 
-@app.get("/book/{book_title}") 
-async def read_book(book_title: str):
+@app.get("/book/{book_name}") 
+async def read_book(book_name: str):
         for book in BOOKS: 
-            if book.get('title').casefold() == book_title.casefold():
+            if book.get('title').casefold() == book_name.casefold():
                 return book
+#################################################################
+
+##### query parameter ########################
+# bade ? query param miyad
+#for example : 127.0.0.1:8000/books/?category=math
+
+# @app.get('/books/') 
+# async def read_category_by_query(category: str):
+#             books_to_return = [] 
+#             for book in BOOKS: 
+#                 if book.get('category').casefold() == category.casefold():
+#                     books_to_return.append(book)
+#             return books_to_return
+
+
+###### using path param with query at same time #######
